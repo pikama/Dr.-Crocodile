93ab763d053d1705c482ec84a96419bc7a9cc343
pikama <34977908+pikama@users.noreply.github.com>
2018-01-25 23:07:42 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace DRcrocodile
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbP1 = new System.Windows.Forms.Label();
+            this.lbP2 = new System.Windows.Forms.Label();
+            this.alert1 = new System.Windows.Forms.Label();
+            this.alert2 = new System.Windows.Forms.Label();
+            this.reset = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.t0 = new DRcrocodile.TransparentControl();
+            this.t9 = new DRcrocodile.TransparentControl();
+            this.t8 = new DRcrocodile.TransparentControl();
+            this.t7 = new DRcrocodile.TransparentControl();
+            this.t6 = new DRcrocodile.TransparentControl();
+            this.t5 = new DRcrocodile.TransparentControl();
+            this.t4 = new DRcrocodile.TransparentControl();
+            this.t3 = new DRcrocodile.TransparentControl();
+            this.t2 = new DRcrocodile.TransparentControl();
+            this.t1 = new DRcrocodile.TransparentControl();
+            this.pbCloseHead = new DRcrocodile.TransparentControl();
+            this.pbOpenHead = new DRcrocodile.TransparentControl();
+            this.pbBase = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBase)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(48, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 34);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Player 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(898, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 34);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Player 2";
+            // 
+            // lbP1
+            // 
+            this.lbP1.AutoSize = true;
+            this.lbP1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbP1.Location = new System.Drawing.Point(120, 288);
+            this.lbP1.Name = "lbP1";
+            this.lbP1.Size = new System.Drawing.Size(21, 24);
+            this.lbP1.TabIndex = 17;
+            this.lbP1.Text = "0";
+            // 
+            // lbP2
+            // 
+            this.lbP2.AutoSize = true;
+            this.lbP2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbP2.Location = new System.Drawing.Point(970, 288);
+            this.lbP2.Name = "lbP2";
+            this.lbP2.Size = new System.Drawing.Size(21, 24);
+            this.lbP2.TabIndex = 18;
+            this.lbP2.Text = "0";
+            // 
+            // alert1
+            // 
+            this.alert1.AutoSize = true;
+            this.alert1.BackColor = System.Drawing.Color.LimeGreen;
+            this.alert1.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.alert1.Location = new System.Drawing.Point(169, 250);
+            this.alert1.Name = "alert1";
+            this.alert1.Size = new System.Drawing.Size(36, 34);
+            this.alert1.TabIndex = 19;
+            this.alert1.Text = "   ";
+            // 
+            // alert2
+            // 
+            this.alert2.AutoSize = true;
+            this.alert2.BackColor = System.Drawing.Color.LimeGreen;
+            this.alert2.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.alert2.Location = new System.Drawing.Point(1019, 250);
+            this.alert2.Name = "alert2";
+            this.alert2.Size = new System.Drawing.Size(36, 34);
+            this.alert2.TabIndex = 20;
+            this.alert2.Text = "   ";
+            // 
+            // reset
+            // 
+            this.reset.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.reset.Location = new System.Drawing.Point(512, 611);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(76, 39);
+            this.reset.TabIndex = 21;
+            this.reset.Text = "Again!";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(50, 288);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 24);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Score:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(900, 288);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 24);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Score:";
+            // 
+            // t0
+            // 
+            this.t0.BackColor = System.Drawing.Color.Transparent;
+            this.t0.Image = global::DRcrocodile.Properties.Resources.t0;
+            this.t0.Location = new System.Drawing.Point(546, 394);
+            this.t0.Name = "t0";
+            this.t0.Size = new System.Drawing.Size(35, 44);
+            this.t0.TabIndex = 25;
+            this.t0.Text = "transparentControl1";
+            // 
+            // t9
+            // 
+            this.t9.BackColor = System.Drawing.Color.Transparent;
+            this.t9.Image = global::DRcrocodile.Properties.Resources.t9;
+            this.t9.Location = new System.Drawing.Point(694, 321);
+            this.t9.Name = "t9";
+            this.t9.Size = new System.Drawing.Size(40, 37);
+            this.t9.TabIndex = 26;
+            this.t9.Text = "transparentControl1";
+            // 
+            // t8
+            // 
+            this.t8.BackColor = System.Drawing.Color.Transparent;
+            this.t8.Image = global::DRcrocodile.Properties.Resources.t8;
+            this.t8.Location = new System.Drawing.Point(735, 336);
+            this.t8.Name = "t8";
+            this.t8.Size = new System.Drawing.Size(38, 39);
+            this.t8.TabIndex = 27;
+            this.t8.Text = "transparentControl1";
+            // 
+            // t7
+            // 
+            this.t7.BackColor = System.Drawing.Color.Transparent;
+            this.t7.Image = global::DRcrocodile.Properties.Resources.t7;
+            this.t7.Location = new System.Drawing.Point(765, 359);
+            this.t7.Name = "t7";
+            this.t7.Size = new System.Drawing.Size(31, 39);
+            this.t7.TabIndex = 28;
+            this.t7.Text = "transparentControl1";
+            // 
+            // t6
+            // 
+            this.t6.BackColor = System.Drawing.Color.Transparent;
+            this.t6.Image = global::DRcrocodile.Properties.Resources.t6;
+            this.t6.Location = new System.Drawing.Point(776, 384);
+            this.t6.Name = "t6";
+            this.t6.Size = new System.Drawing.Size(35, 41);
+            this.t6.TabIndex = 29;
+            this.t6.Text = "transparentControl1";
+            // 
+            // t5
+            // 
+            this.t5.BackColor = System.Drawing.Color.Transparent;
+            this.t5.Image = global::DRcrocodile.Properties.Resources.t5;
+            this.t5.Location = new System.Drawing.Point(755, 406);
+            this.t5.Name = "t5";
+            this.t5.Size = new System.Drawing.Size(36, 44);
+            this.t5.TabIndex = 30;
+            this.t5.Text = "transparentControl1";
+            // 
+            // t4
+            // 
+            this.t4.BackColor = System.Drawing.Color.Transparent;
+            this.t4.Image = global::DRcrocodile.Properties.Resources.t4;
+            this.t4.Location = new System.Drawing.Point(717, 425);
+            this.t4.Name = "t4";
+            this.t4.Size = new System.Drawing.Size(42, 44);
+            this.t4.TabIndex = 31;
+            this.t4.Text = "transparentControl1";
+            // 
+            // t3
+            // 
+            this.t3.BackColor = System.Drawing.Color.Transparent;
+            this.t3.Image = global::DRcrocodile.Properties.Resources.t3;
+            this.t3.Location = new System.Drawing.Point(669, 433);
+            this.t3.Name = "t3";
+            this.t3.Size = new System.Drawing.Size(43, 43);
+            this.t3.TabIndex = 32;
+            this.t3.Text = "transparentControl1";
+            // 
+            // t2
+            // 
+            this.t2.BackColor = System.Drawing.Color.Transparent;
+            this.t2.Image = global::DRcrocodile.Properties.Resources.t2;
+            this.t2.Location = new System.Drawing.Point(617, 429);
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(42, 46);
+            this.t2.TabIndex = 33;
+            this.t2.Text = "transparentControl1";
+            // 
+            // t1
+            // 
+            this.t1.BackColor = System.Drawing.Color.Transparent;
+            this.t1.Image = global::DRcrocodile.Properties.Resources.t1;
+            this.t1.Location = new System.Drawing.Point(572, 416);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(42, 47);
+            this.t1.TabIndex = 34;
+            this.t1.Text = "transparentControl1";
+            // 
+            // pbCloseHead
+            // 
+            this.pbCloseHead.BackColor = System.Drawing.Color.Transparent;
+            this.pbCloseHead.Image = global::DRcrocodile.Properties.Resources.cCloseHead;
+            this.pbCloseHead.Location = new System.Drawing.Point(250, 5);
+            this.pbCloseHead.Name = "pbCloseHead";
+            this.pbCloseHead.Size = new System.Drawing.Size(600, 600);
+            this.pbCloseHead.TabIndex = 36;
+            this.pbCloseHead.Text = "transparentControl2";
+            // 
+            // pbOpenHead
+            // 
+            this.pbOpenHead.BackColor = System.Drawing.Color.Transparent;
+            this.pbOpenHead.Image = global::DRcrocodile.Properties.Resources.cOpenHead;
+            this.pbOpenHead.Location = new System.Drawing.Point(250, 5);
+            this.pbOpenHead.Name = "pbOpenHead";
+            this.pbOpenHead.Size = new System.Drawing.Size(600, 600);
+            this.pbOpenHead.TabIndex = 35;
+            this.pbOpenHead.Text = "transparentControl1";
+            // 
+            // pbBase
+            // 
+            this.pbBase.BackColor = System.Drawing.Color.Transparent;
+            this.pbBase.BackgroundImage = global::DRcrocodile.Properties.Resources.cBase1;
+            this.pbBase.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbBase.Location = new System.Drawing.Point(250, 5);
+            this.pbBase.Name = "pbBase";
+            this.pbBase.Size = new System.Drawing.Size(600, 600);
+            this.pbBase.TabIndex = 37;
+            this.pbBase.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 662);
+            this.Controls.Add(this.t0);
+            this.Controls.Add(this.t9);
+            this.Controls.Add(this.t8);
+            this.Controls.Add(this.t7);
+            this.Controls.Add(this.t6);
+            this.Controls.Add(this.t5);
+            this.Controls.Add(this.t4);
+            this.Controls.Add(this.t3);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.pbCloseHead);
+            this.Controls.Add(this.pbOpenHead);
+            this.Controls.Add(this.pbBase);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.alert2);
+            this.Controls.Add(this.alert1);
+            this.Controls.Add(this.lbP2);
+            this.Controls.Add(this.lbP1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Dr. Crocodile";
+            ((System.ComponentModel.ISupportInitialize)(this.pbBase)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbP1;
+        private System.Windows.Forms.Label lbP2;
+        private System.Windows.Forms.Label alert1;
+        private System.Windows.Forms.Label alert2;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private TransparentControl t0;
+        private TransparentControl t9;
+        private TransparentControl t8;
+        private TransparentControl t7;
+        private TransparentControl t6;
+        private TransparentControl t5;
+        private TransparentControl t4;
+        private TransparentControl t3;
+        private TransparentControl t2;
+        private TransparentControl t1;
+        private TransparentControl pbOpenHead;
+        private TransparentControl pbCloseHead;
+        private System.Windows.Forms.PictureBox pbBase;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DRcrocodile
+{
+    public partial class MainForm : Form
+    {
+
+        TransparentControl[] teeth;
+        string randomTeeth;
+        string turn;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;           
+
+            //init
+            reset.Visible = false;
+            pbCloseHead.Visible = false;
+
+            teeth = new TransparentControl[10] { t0, t1, t2, t3, t4, t5, t6, t7, t8, t9 };
+            
+            foreach (TransparentControl t in teeth)
+            {
+                t.Click += ClickTeeth;                
+            }
+
+            
+
+            turn = "p1"; // player 1 go first
+            alert1.BackColor = Color.LimeGreen;
+            alert2.BackColor = Color.Transparent;
+
+            randomTeeth = "t" + (new Random()).Next(10);
+
+        }
+
+        
+        void ClickTeeth(object sender, EventArgs e)
+        {
+            TransparentControl pb_click = sender as TransparentControl;
+            pb_click.Visible = false;
+            if (pb_click.Name == randomTeeth)
+            {
+                if (turn == "p1")
+                {
+                    lbP2.Text = (Int32.Parse(lbP2.Text) + 1).ToString();
+                }
+                else
+                {
+                    lbP1.Text = (Int32.Parse(lbP1.Text) + 1).ToString();
+                }
+
+                foreach (TransparentControl t in teeth)
+                {
+                    t.Visible = false;
+
+                }
+                pbCloseHead.Visible = true;
+                pbOpenHead.Visible = false;
+                reset.Visible = true;
+            }
+            else
+            {
+                changePlayer();
+            }           
+        }
+        
+        private void reset_Click(object sender, EventArgs e)
+        {
+            //reset all setting
+            pbCloseHead.Visible = false;
+            pbOpenHead.Visible = true;
+            reset.Visible = false;
+            foreach (TransparentControl t in teeth)
+            {
+                t.Visible = true;
+            }
+            randomTeeth = "t" + (new Random()).Next(10);
+            changePlayer();
+        }
+        
+        private void changePlayer()
+        {
+            turn = (turn == "p1") ? "p2" : "p1";
+            alert1.BackColor = alert1.BackColor == Color.LimeGreen ? Color.Transparent : Color.LimeGreen;
+            alert2.BackColor = alert2.BackColor == Color.LimeGreen ? Color.Transparent : Color.LimeGreen;
+        }
+        
+    }
+}
